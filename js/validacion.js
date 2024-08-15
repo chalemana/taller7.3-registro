d53231d114d7b6bb444458c046d7e470a95e94fb
unknown <tefiiiminutti99@gmail.com>
2024-08-15 14:18:09 -0300

    if casi echo

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,15 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+let password1 = document.getElementById("password1");
+let password2 = document.getElementById("password2");
+let checkbox = document.getElementById("terminos");
+
+if (password1.length>= 6);
+
+
+
+
+
